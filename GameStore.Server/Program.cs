0a94f4f35d663618f95f45d91f1432b6234f1bba
Iain Stewart <iainstewart08@gmail.com>
2024-01-05 16:59:02 +0000

    Add GET game by id endpoint

--- GameStore.Server/Program.cs
+++ GameStore.Server/Program.cs
@@ -31,6 +31,18 @@ List<Game> games = new()
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// GET /games
 app.MapGet("/games", () => games);
+
+// GET /games/{id}
+app.MapGet("/games/{id}", (int id) => 
+{
+    Game? game = games.Find(x => x.Id == id);
+    if (game is null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(game);
+});
 
 app.Run();
